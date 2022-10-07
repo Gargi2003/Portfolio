abcf0cfb7eee6604bafe1c1dbdfef736ee328fb7
Bandyopadhyay <gargi.bandyopadhyay@accenture.com>
2022-10-07 17:29:31 +0530

    layout changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
