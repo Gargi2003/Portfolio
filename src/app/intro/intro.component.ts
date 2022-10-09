7e5cb2c32bb1cb2256fbfbc06bdd7e51d1898add
Bandyopadhyay <gargi.bandyopadhyay@accenture.com>
2022-10-10 00:13:02 +0530

    made changes to typewriter

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,15 +1,106 @@
-import { Component, OnInit } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  Renderer2,
+  ViewChild
+} from "@angular/core";
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
-export class IntroComponent implements OnInit {
+export class IntroComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild("textElement")
+  textElement!: ElementRef;
+  @ViewChild("blinkElement") blinkElement: ElementRef | any;
 
-  ngOnInit(): void {
+  @Input() wordArray: string[] = [
+    "Software Engineer",
+    "Fullstack Developer",
+    "Learner",
+    "Coder"
+  ];
+  @Input() textColor = "#FC6D6D";
+  @Input() fontSize = "30px";
+  @Input() blinkWidth = "2px";
+  @Input() typingSpeedMilliseconds = 100;
+  @Input() deleteSpeedMilliseconds = 100;
+
+  private i = 0;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.initVariables();
+    this.typingEffect();
+  }
+
+  private initVariables(): void {
+    this.renderer.setStyle(
+      this.textElement.nativeElement,
+      "color",
+      this.textColor
+    );
+    this.renderer.setStyle(
+      this.textElement.nativeElement,
+      "font-size",
+      this.fontSize
+    );
+    this.renderer.setStyle(this.textElement.nativeElement, "padding", "0.1em");
+
+    this.renderer.setStyle(
+      this.blinkElement.nativeElement,
+      "border-right-width",
+      this.blinkWidth
+    );
+    this.renderer.setStyle(
+      this.blinkElement.nativeElement,
+      "border-right-color",
+      this.textColor
+    );
+    this.renderer.setStyle(
+      this.blinkElement.nativeElement,
+      "font-size",
+      this.fontSize
+    );
+  }
+
+  private typingEffect(): void {
+    const word = this.wordArray[this.i].split("");
+    const loopTyping = () => {
+      if (word.length > 0) {
+        this.textElement.nativeElement.innerHTML += word.shift();
+      } else {
+        this.deletingEffect();
+        return;
+      }
+      setTimeout(loopTyping, this.typingSpeedMilliseconds);
+    };
+    loopTyping();
+  }
+
+  private deletingEffect(): void {
+    const word = this.wordArray[this.i].split("");
+    const loopDeleting = () => {
+      if (word.length > 0) {
+        word.pop();
+        this.textElement.nativeElement.innerHTML = word.join("");
+      } else {
+        if (this.wordArray.length > this.i + 1) {
+          this.i++;
+        } else {
+          this.i = 0;
+        }
+        this.typingEffect();
+        return false;
+      }
+      setTimeout(loopDeleting, this.deleteSpeedMilliseconds);
+    };
+    loopDeleting();
   }
 
 }
